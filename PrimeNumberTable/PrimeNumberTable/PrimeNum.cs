00b6c445ce9a9658531b924f0117ef4304148339
TJCUPP <tyl,cupp@gmail.com>
2016-08-12 02:25:07 +0100

    Add files via upload

---
+++ PrimeNumberTable/PrimeNumberTable/PrimeNum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberTable
+{
+    public class PrimeNum
+    {
+        public static List<int> PrimeGen(int Prime)
+        {
+           
+            List<int> primes = new List<int>();
+
+            primes.Add(2);
+            int nextPrime = 3;
+            while (primes.Count < Prime)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                bool isPrime = true;
+                for (int i = 0; (int)primes[i] <= sqrt; i++)
+                {
+                    if (nextPrime % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(nextPrime);
+                }
+                nextPrime += 2;
+            }
+            return primes;
+        }
+
+        public static void PrimeTable(List<int> primenumbers)
+        {
+            int largestNumber = primenumbers.ElementAt(primenumbers.Count - 1);
+            string format = "{0," + ((largestNumber * largestNumber).ToString().Length + 1).ToString() + "} ";
+
+            Console.Write(string.Format(format, ""));
+
+            foreach (int x in primenumbers)
+            {
+                Console.Write(string.Format(format, x));
+            }
+
+            Console.WriteLine();
+            foreach (int x in primenumbers)
+            {
+                Console.Write(string.Format(format, x));
+                foreach (int y in primenumbers)
+                { 
+                    Console.Write(string.Format(format, x * y));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
---
+++ PrimeNumberTable/PrimeNumberTable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PrimeNumberTable
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter whole number that is equal or greater to 1");
+            string primeAsString = Console.ReadLine();
+            int Prime = int.Parse(primeAsString);
+            
+
+            if (Prime > 0)
+            {
+                var primeNum = PrimeNum.PrimeGen(Prime);
+                PrimeNum.PrimeTable(primeNum);
+            }
+
+            else
+            {
+                Console.Write("Sorry but 0 isn't greater than one. Please reload and try again!. \n\n\n Goodbye! \n");
+            }
+            
+        }
+    }
+}
